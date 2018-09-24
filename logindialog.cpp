846faf017e25373437d0855cfb3bd6c1f0099123
Muhammed Emin ÖMÜR <eminomur@users.noreply.github.com>
2018-09-24 03:16:41 +0300

    Update logindialog.cpp

--- logindialog.cpp
+++ logindialog.cpp
@@ -13,9 +13,7 @@ LoginDialog::LoginDialog(QWidget *parent) :
 {
     ui->setupUi(this);
     // To connect database
-    connection = new MongoDBConnection("mongodb://xyz:1@cluster0-shard-00-00-ohmfr.gcp.mongodb.net:27017,cluster0-shard-00-01-ohmfr.gcp.mongodb.net:27017,"
-                                       "cluster0-shard-00-02-ohmfr.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&"
-                                       "retryWrites=true", "calendar", "userlist");
+    connection = new MongoDBConnection(/* Enter your connection parameter here */);
 }
 
 LoginDialog::~LoginDialog()
