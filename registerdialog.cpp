721a7867052ffe4e79863ce24c8bb4f308a4f38a
Muhammed Emin ÖMÜR <eminomur@users.noreply.github.com>
2018-09-24 03:17:18 +0300

    Update registerdialog.cpp

--- registerdialog.cpp
+++ registerdialog.cpp
@@ -10,9 +10,7 @@ RegisterDialog::RegisterDialog(QWidget *parent) :
 {
     ui->setupUi(this);
     // To connect database
-    connection = new MongoDBConnection("mongodb://xyz:1@cluster0-shard-00-00-ohmfr.gcp.mongodb.net:27017,cluster0-shard-00-01-ohmfr.gcp.mongodb.net:27017,"
-                                       "cluster0-shard-00-02-ohmfr.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&"
-                                       "retryWrites=true", "calendar", "userlist");
+    connection = new MongoDBConnection(/* Enter your connection parameter here */);
 }
 
 RegisterDialog::~RegisterDialog()
